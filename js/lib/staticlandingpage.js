3d40f0f80a0628b1e5914ad0479cb0dec99cbbb6
avryabadd <ajabad42@tntech.edu>
2025-03-13 12:06:37 -0500

    launch into ui

--- js/lib/staticlandingpage.js
+++ js/lib/staticlandingpage.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btnStaticSwap = document.querySelector('#btnNewRegister');
-
     if (btnStaticSwap) {
         btnStaticSwap.addEventListener('click', () => {
             window.location.href = 'components/register.html';
         });
     }
 });
+
+document.querySelector('#btnLogin').addEventListener('click', (e) => {
+
+    window.location.href = '../components/ui.html'
+})
 
